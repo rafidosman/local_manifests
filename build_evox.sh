8a13080fda5ff72c9b612aac804a61d7b6625eff
fido <rafidosman17@gmail.com>
2023-04-09 00:32:30 +0800

    added evox build script

---
+++ build_evox.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+
+# Build script for my build "for personal usage only"
+
+devices=(
+    "pstar"
+)
+
+## Sync
+# Init Evolution-X repos
+repo init -u https://github.com/Evolution-X/manifest -b tiramisu
+
+#remove sync project from Evolution-X manifest
+sed -i "/\platform\/hardware\/qcom\/sm7250\/media/d" .repo/manifests/default.xml
+sed -i "/\platform\/hardware\/qcom\/sm7250\/display/d" .repo/manifests/default.xml
+sed -i "/\platform\/hardware\/qcom\/sm8150\/media/d" .repo/manifests/default.xml
+sed -i "/\platform\/hardware\/qcom\/sm8150\/display/d" .repo/manifests/default.xml
+sed -i "/\platform\/hardware\/qcom\/sm8150\/data\/ipacfg-mgr/d" .repo/manifests/default.xml
+sed -i "/\platform\/hardware\/google\/pixel/d" .repo/manifests/default.xml
+sed -i "/\platform\/hardware\/google\/pixel-sepolicy/d" .repo/manifests/default.xml
+
+# Add local_manifests
+rm -rf .repo/local_manifests
+mkdir -p .repo/local_manifests
+curl https://raw.githubusercontent.com/rafidosman/local_manifests/main/evo.xml -o .repo/local_manifests/evo.xml
+
+# Sync
+repo sync -c --force-sync --no-clone-bundle --no-tags -j$(nproc --all)
+
+## Build
+# Init envsetup
+#. vendor/extra/build/envsetup.sh -p
+# Build
+#for device in ${devices[@]}; do
+#    echo "Build for ${device}"
+#    sleep 3
+#    mka_build ${device} -r
+#done
+
