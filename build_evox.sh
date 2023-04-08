7b24919fa2097a1821e722e71f5faa214dc38a38
fido <rafidosman17@gmail.com>
2023-04-09 01:04:13 +0800

    updated evox build script

--- build_evox.sh
+++ build_evox.sh
@@ -1,24 +1,28 @@
 #!/bin/bash
 
 # Build script for my build "for personal usage only"
-
-START=`date +%s`
-BUILD_DATE="$(date +%Y%m%d)"
+set -e
 
 devices=(
     "pstar"
 )
 
+initRepos() {
 ## Sync
 # Init Evolution-X repos
+echo "-->> repo init"
 repo init -u https://github.com/Evolution-X/manifest -b tiramisu
+echo
 
 #remove sync project from Evolution-X manifest
+echo "remove unwanted project"
 sed -i "/\platform\/hardware\/qcom\/sm7250\/media/d" .repo/manifests/default.xml
 sed -i "/\platform\/hardware\/qcom\/sm7250\/display/d" .repo/manifests/default.xml
 sed -i "/\platform\/hardware\/qcom\/sm8150\/media/d" .repo/manifests/default.xml
 sed -i "/\platform\/hardware\/qcom\/sm8150\/display/d" .repo/manifests/default.xml
 sed -i "/\platform\/hardware\/qcom\/sm8150\/data\/ipacfg-mgr/d" .repo/manifests/default.xml
+sed -i "/\hardware\/qcom\/sm8150\/Android.mk/d" .repo/manifests/default.xml
+sed -i "/\hardware\/qcom\/sm8150\/Android.bp/d" .repo/manifests/default.xml
 sed -i "/\platform\/hardware\/google\/pixel/d" .repo/manifests/default.xml
 sed -i "/\platform\/hardware\/google\/pixel-sepolicy/d" .repo/manifests/default.xml
 
@@ -26,17 +30,31 @@ sed -i "/\platform\/hardware\/google\/pixel-sepolicy/d" .repo/manifests/default.
 rm -rf .repo/local_manifests
 mkdir -p .repo/local_manifests
 curl https://raw.githubusercontent.com/rafidosman/local_manifests/main/evo.xml -o .repo/local_manifests/evo.xml
+}
 
+
+syncRepos() {
 # Sync
+echo "-->> syncing repos"
 repo sync -c --force-sync --no-clone-bundle --no-tags -j$(nproc --all)
+echo
+}
 
+setupEnv() {
 ## Build
-# Init envsetup
+echo "-->> setting up envsetup"
 #. vendor/extra/build/envsetup.sh -p
-. build/envsetup.sh
+source build/envsetup.sh
+echo
+}
+
+buildRom() {
+echo "Building rom"
 lunch evolution_redfin_userdebug
 make -j$(nproc --all) installclean
 mka evolution
+echo
+}
 # Build
 #for device in ${devices[@]}; do
 #    echo "Build for ${device}"
@@ -44,9 +62,18 @@ mka evolution
 #    mka_build ${device} -r
 #done
 
+START=`date +%s`
+BUILD_DATE="$(date +%Y%m%d)"
+
+initRepos
+syncRepos
+setupEnv
+buildRom
+
+
 END=`date +%s`
 ELAPSEDM=$(($(($END-$START))/60))
 ELAPSEDS=$(($(($END-$START))-$ELAPSEDM*60))
 
-echo "--> Build script completed in $ELAPSEDM minutes and $ELAPSEDS seconds"
+echo "--> Build script for $devices completed in $ELAPSEDM minutes and $ELAPSEDS seconds"
 echo
