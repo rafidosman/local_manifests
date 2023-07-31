0cf102b104a73b5463b5e3d17609d21b428d3ad8
fido <rafidosman17@gmail.com>
2023-07-31 09:21:16 +0800

    remove notification repopick

--- build_lineage.sh
+++ build_lineage.sh
@@ -63,9 +63,6 @@ echo
 applyPatches() {
 echo "-->> Applying personal patches"
 
-repopick 321337 -f # Deprioritize important developer notifications
-repopick 321338 -f # Allow disabling important developer notifications
-repopick 321339 -f # Allow disabling USB notifications
 repopick 340916 # SystemUI: add burnIn protection
 repopick 342860 # codec2: Use numClientBuffers to control the pipeline
 repopick 342861 # CCodec: Control the inputs to avoid pipeline overflow
